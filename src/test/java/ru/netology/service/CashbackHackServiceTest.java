3f4002a3d4e6baa5f57ed435e0e4e2c0adb8ed67
Maksim Mishin <mihsin_ma29@mail.ru>
2023-03-02 23:16:58 +0300

    3

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,62 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testRemain() {
+        int amount = 900;
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Remain1000() {
+        int amount = 1000;
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Remain1001() {
+        int amount = 1001;
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Remain999() {
+        int amount = 999;
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Remain1() {
+        int amount = 1;
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    
+}
